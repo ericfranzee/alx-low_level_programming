33eef5df2215d0c6cb4f2bc0aa74539262a9cf0b
ericfranzee <ericfranzee@gmail.com>
2023-02-21 16:04:04 -0800

    functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is a simple c programme
+ * that prints the word _putchar
+ * using _putchar and a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
