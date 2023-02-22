d3eefdcb889c7c75607bb0d56864ab2fc0247d29
ericfranzee <ericfranzee@gmail.com>
2023-02-21 17:21:11 -0800

    functions nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,19 @@ int main(void)
 {
 	int counter;
 	long a = 0;
-	long b = 1;
+	long b = 1, sum;
 
 	for (counter = 0; counter < 50; counter++)
 	{
-		printf("%li %li", a, b);
-		a += b;
-		b += a;
+		sum = a + b;
+		printf("%li", sum);
+		a = b;
+		b = sum;
 
 	if (counter == 49)
 		printf("\n");
 	else
 		printf(", ");
-}
+	}
 	return (0);
 }
