01dda5b339dcd715562c81927298ddcc349fe975
ericfranzee <ericfranzee@gmail.com>
2023-03-08 14:11:10 -0800

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,55 @@
 #include "main.h"
+int palcheck(char *c, int len, int i);
 /**
- * substring_match - check if a substring after wildcard matches s1
- * @s1: one string
- * @s2: one string
- * @after_wldcd: placeholder for position right after wildcard
- * Return: 1 if matched, 0 if not
+ * is_palindrome - calls funcions to check length then check if palindorme
+ * @s:string
+ * Return:1 if palindorme 0 if not
+ *
+ *
  */
-
-int substring_match(char *s1, char *s2, char *after_wldcd)
+int is_palindrome(char *s)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == '\0' && *s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 + 1));
-	if (*s1 == '\0' && *s2 != '\0')
-		return (0);
-	if (*s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 + 1));
-	if (*s1 == *s2)
-		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
-	else
-		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+	int l, i;
+
+	i = 0;
+	l =  _strlen_recursion(s);
+
+	return (palcheck(s, l, i));
 }
 
 /**
- * wildcmp - compare if string with wildcard mattches
- * @s1: one string
- * @s2: one string
- * Return: 1 if matched, 0 if not
+ * palcheck - check if palindorme
+ * @c:string
+ * @len: length of string
+ * @i: index
+ * Return:1 if palindorme 0 if not
  */
-
-int wildcmp(char *s1, char *s2)
+int palcheck(char *c, int len, int i)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (c[i] == c[len / 2])
+	{
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
-		return (substring_match(s1, (s2 + 1), (s2 + 1)));
-	else
-		return (0);
+	}
+	if (c[i] == c[len - i - 1])
+	{
+		return (palcheck(c, len, i + 1));
+	}
+	return (0);
+}
+/**
+ * _strlen_recursion - messurse string
+ * @s:string to messure
+ * Return:length
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+	if (*s != '\0')
+	{
+		len++;
+		return (len + _strlen_recursion(s + 1));
+	}
+	return (len);
 }
