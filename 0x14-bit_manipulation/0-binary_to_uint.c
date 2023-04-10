261c01f7799216a55660b02f6ac678ecc27a9a1b
ericfranzee <ericfranzee@gmail.com>
2023-04-10 06:34:32 -0700

    Git Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
-	int length, base_two;
+	int str_len, base_two;
 
 	if (!b)
 		return (0);
 
 	ui = 0;
 
-	for (length = 0; b[length] != '\0'; length++)
+	for (str_len = 0; b[str_len] != '\0'; str_len++)
 		;
 
-	for (length--, base_two = 1; length >= 0; length--, base_two *= 2)
+	for (str_len--, base_two = 1; str_len >= 0; str_len--, base_two *= 2)
 	{
-		if (b[length] != '0' && b[length] != '1')
+		if (b[str_len] != '0' && b[str_len] != '1')
 		{
 			return (0);
 		}
 
-		if (b[length] & 1)
+		if (b[str_len] & 1)
 		{
 			ui += base_two;
 		}
